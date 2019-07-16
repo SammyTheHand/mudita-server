fc51aeaeb9e3e435e418d172a73305662a7aff49
Sam Smallman <srsmallman@mac.com>
2019-07-16 18:44:58 +0100

    Simplify Javascript

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,4 @@
-var video1 = videojs('video1', {
-	playbackRates: [.5, 1, 1.5, 2, 2.5],
-	fluid: true,
-	plugins: {
-		hotkeys: {
-    		volumeStep: 0.1,
-    		seekStep: 5,
-    		enableNumbers: false,
-    		enableModifiersForNumbers: false
-  		}
-	}
-});
-
-var video2 = videojs('video2', {
-	playbackRates: [.5, 1, 1.5, 2, 2.5],
-	fluid: true,
-	plugins: {
-		hotkeys: {
-    		volumeStep: 0.1,
-    		seekStep: 5,
-    		enableNumbers: false,
-    		enableModifiersForNumbers: false
-  		}
-	}
-});
-
-var video3 = videojs('video3', {
+var video = videojs('video', {
 	playbackRates: [.5, 1, 1.5, 2, 2.5],
 	fluid: true,
 	plugins: {
